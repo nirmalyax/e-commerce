2e0d6c6b3b17df04a3b29b022fc03fdd3b1f4c83
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-12-02 14:48:23 +0530

    @nirmalyax Sat 2 Dec 2023

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>React App</h1>
+          <p>My first React App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

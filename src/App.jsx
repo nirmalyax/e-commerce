eb2ed89d6c83facc6ff29030f8aeb6af3aac6df5
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-12-02 15:40:24 +0530

    @nirmalyax Sat 2 Dec 2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <h1>React App</h1>
-          <p>My first React App</p>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/order" element={<Order/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/*" element={<NoPage/>} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/allproducts/AllProducts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../../components/layout/Layout'
+
+function AllProducts() {
+  return (
+    <Layout>AllProducts</Layout>
+  )
+}
+
+export default AllProducts

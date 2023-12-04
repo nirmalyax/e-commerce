c9f4fae99f2b2d887fd154d7a5c3e733b3c50b0a
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-12-04 17:52:38 +0530

    Navbar updtae with tailwind component

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from "../navbar/Navbar";
+import  Navbar  from "../navbar/Navbar";
 import { Footer } from "../footer/Footer";
 
 const Layout = ({ children }) => {
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,12 @@
-import React, { useContext } from "react";
+import React from "react";
 import Layout from "../../components/layout/Layout";
-import myContext from "../../context/data/myContext";
+//import myContext from "../../context/data/myContext";
 
 function Home() {
-  const context = useContext(myContext);
-  console.log(context);
-  const { name, roll } = context;
+  
   return (
     <Layout>
-      <h1>Name : {name}</h1>
-      <h2>Roll : {roll}</h2>
+      <h1>Home</h1>
     </Layout>
   );
 }
